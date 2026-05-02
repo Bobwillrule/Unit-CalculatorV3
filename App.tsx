e46bf0ef0e89d82e12f9bc74dc6e42e7f11df1f2
Hugo Chen <hugohqchen@gmail.com>
2026-05-01 18:01:48 -0700

    global config

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import {
+    SafeAreaView,
+    StatusBar,
+    StyleSheet
+} from 'react-native';
+
+import { HomeScreen } from './src/screens/HomeScreen';
+
+const App = () =>{
+    return (
+        <SafeAreaView style = {styles.container}>
+            <StatusBar barStyle = "dark-content"/>
+            <HomeScreen />
+
+        </SafeAreaView>
+    );
+    
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
+
+export default App
